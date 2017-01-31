a35417c88b6b4f62a7ce09a17ba8b614d356c139
David Suarez <dsuarez4@terpmail.umd.edu>
2017-01-30 21:01:39 -0500

    container-code: initial commit

---
+++ src/main/java/core/HiearchyDefinition.java
@@ -0,0 +1,13 @@
+package core;
+
+/**
+ * Created by davidsuarez on 1/26/17.
+ */
+public class HiearchyDefinition {
+
+
+    public String id;
+    public String[] parents;
+    public String[] children;
+
+}
